db20ef1f786875fa0ba5c208a9b9f23f10aa54bb
exmcopi <milton.corvera@externos.bci.cl>
2023-09-19 14:22:08 -0600

    configure token and login Oauth2 keyCloak

--- src/main/java/com/mcorvera/mibanca/gatewayservice/config/SecurityConfig.java
+++ src/main/java/com/mcorvera/mibanca/gatewayservice/config/SecurityConfig.java
@@ -1,24 +1,31 @@
 package com.mcorvera.mibanca.gatewayservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("**/actuator/**").permitAll()
-                .antMatchers("**/test-prop/**").permitAll()
-                .and()
-                .authorizeRequests().anyRequest().authenticated();
-        httpSecurity.oauth2ResourceServer()
-                .jwt();
+                .authorizeExchange(
+                        exchanges -> exchanges
+                                .pathMatchers("**/actuator/**").permitAll()
+                                .pathMatchers(HttpMethod.GET,"**/test-prop").permitAll()
+                                .pathMatchers("**/swagger-ui.html", "**/swagger-ui/**").permitAll()
+                                .anyExchange().authenticated()
+                );
+        httpSecurity.oauth2ResourceServer().jwt();
+        httpSecurity.oauth2Login();
         return httpSecurity.build();
     }
 }
